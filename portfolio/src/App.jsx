8bb8de4b202d311300d97acc0f46cec7be9d9aa3
Nebiyu14 <tesfayenebiyu3@gmail.com>
2026-04-06 16:58:40 +0200

    added the loading effect

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -9,9 +9,18 @@ import Projects from "./components/projects/Projects";
 import Contact from "./components/contact/Contact";
 import Footer from "./components/footer/Footer";
 import ScrollToTop from "./components/scrollToTop/ScrollToTop";
+import Loader from "./components/loader/Loader";
 
 function App() {
   const [isDark, setIsDark] = useState(true);
+  const [loading, setLoading] = useState(true);
+
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      setLoading(false);
+    }, 1000);
+    return () => clearTimeout(timer);
+  }, []);
   useEffect(() => {
     const html = document.documentElement;
     if (isDark)
@@ -29,33 +38,37 @@ function App() {
 
   return (
     <>
-      <div>
-        <Header
-          isDark={isDark}
-          toggleTheme={toggleTheme}
-          scrollToSection={scrollToSection}
-        />
-        <section id="home">
-          <Hero scrollToSection={scrollToSection} />
-        </section>
-        <section id="about">
-          <About />
-        </section>
-        <section id="skills">
-          <Skills />
-        </section>
-        <section id="services">
-          <Services />
-        </section>
-        <section id="projects">
-          <Projects />
-        </section>
-        <section id="contact">
-          <Contact />
-        </section>
-        <Footer scrollToSection={scrollToSection} />
-        <ScrollToTop />
-      </div>
+      {loading ? (
+        <Loader />
+      ) : (
+        <div>
+          <Header
+            isDark={isDark}
+            toggleTheme={toggleTheme}
+            scrollToSection={scrollToSection}
+          />
+          <section id="home">
+            <Hero scrollToSection={scrollToSection} />
+          </section>
+          <section id="about">
+            <About />
+          </section>
+          <section id="skills">
+            <Skills />
+          </section>
+          <section id="services">
+            <Services />
+          </section>
+          <section id="projects">
+            <Projects />
+          </section>
+          <section id="contact">
+            <Contact />
+          </section>
+          <Footer scrollToSection={scrollToSection} />
+          <ScrollToTop />
+        </div>
+      )}
     </>
   );
 }
